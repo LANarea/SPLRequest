bb424391b73f4921c9c4ea21977e897b5539f0fe
Ken Verhaegen <contact@kenverhaegen.be>
2014-12-28 06:19:24 +0100

    Oversimplification step 2

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ use LANarea;
 
 require __DIR__ . DS . 'src' . DS . 'spl.class.php';
 
-$config_file_path = __DIR__ . DS . 'spl.config.php';
+$spl = new LANarea\SPLRequest('0.0.0.0',0);
 
-$spl = new LANarea\SPLRequest($config_file_path);
-echo $spl->buildLibrary($spl->config['libdir']);
+// Note: Might get rough with big folders...
+echo '<pre>', var_dump($spl->getAllSongs()), '</pre>';
